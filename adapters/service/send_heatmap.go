20a4282add700b0dc3617a577f426d87f8b62f79
dockerdavid <jdgomezdelgado@uts.edu.co>
2024-10-19 16:43:54 -0500

    mandar los eventos, hace falta el back, aparentemente ya todo está ok

--- adapters/service/send_heatmap.go
+++ adapters/service/send_heatmap.go
@@ -1,35 +1,52 @@
 package heatmapServiceAdapter
 
-func (s Service) SendHeatmap() error {
-	readClient, err := s.Repository.ReadClient()
+import (
+	"fmt"
+	"github.com/robfig/cron/v3"
+)
+
+func (s Service) SendHeatmap(TimeToBeSent string) error {
+	c := cron.New()
+
+	_, err := c.AddFunc(fmt.Sprintf("@every %s", TimeToBeSent), func() {
+		fmt.Println("Sending heatmaps")
+		readClient, err := s.Repository.ReadClient()
+
+		if err != nil {
+			return
+		}
+
+		readDevice, err := s.Repository.ReadDevice()
+
+		if err != nil {
+			return
+		}
+
+		readCaptures, err := s.Repository.ReadCaptures()
+
+		if err != nil {
+			return
+		}
+
+		if len(readCaptures) == 0 {
+			return
+		}
+
+		if err = s.Repository.UpdateCaptures(); err != nil {
+			return
+		}
+
+		if err = s.HTTP.SendHeatmaps(readClient, readDevice, readCaptures); err != nil {
+			return
+		}
+
+	})
 
 	if err != nil {
 		return err
 	}
 
-	readDevice, err := s.Repository.ReadDevice()
+	c.Start()
 
-	if err != nil {
-		return err
-	}
-
-	readCaptures, err := s.Repository.ReadCaptures()
-
-	if err != nil {
-		return err
-	}
-
-	if len(readCaptures) == 0 {
-		return nil
-	}
-
-	if err = s.Repository.UpdateCaptures(); err != nil {
-		return err
-	}
-
-	if err = s.HTTP.SendHeatmaps(readClient, readDevice, readCaptures); err != nil {
-		return err
-	}
-
-	return nil
+	select {}
 }
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.23.2
 require github.com/mattn/go-sqlite3 v1.14.24
 
 require (
+	github.com/robfig/cron/v3 v3.0.0 // indirect
 	golang.org/x/mobile v0.0.0-20241016134751-7ff83004ec2c // indirect
 	golang.org/x/mod v0.21.0 // indirect
 	golang.org/x/sync v0.8.0 // indirect
